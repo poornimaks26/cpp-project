8ba4827bdc25e915a1cb94e2b363b9febec78046
poornimaks26 <poornimaks97@gmail.com>
2026-06-19 13:15:48 +0530

    Add sorting using vector

---
+++ sorting_vector.cpp
@@ -0,0 +1,109 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+class selection
+{
+ public:
+ 
+ /*vector<int> selectionsort(vector<int> arr)
+ {
+    int n = arr.size();
+
+    for(int i=0;i<=n-1;i++)
+    {
+        int min =i;
+        for(int j=i;j<=n-1;j++)
+        {
+            if(arr[j]<arr[min])
+            {
+                min=j;
+            }
+
+        }
+        swap(arr[min],arr[i]);
+    }
+    return arr;
+ }
+
+ vector<int> bubblesort(vector<int>& arr)
+ {
+int n= arr.size();
+
+    for(int i=n-1;i>=1;i--)
+{
+   for(int j=0;j<=i-1;j++)
+   {
+    if(arr[j]>arr[j+1])
+    {
+        swap(arr[j],arr[j+1]);
+    }
+   }
+}
+return arr;
+ }*/
+
+ vector<int> insertionsort(vector<int>& nums)
+ {
+
+    int n=nums.size();
+    for(int i=1;i<n;i++)
+    {
+        int key = nums[i];
+        int j = i-1;
+        while(j>=0 && nums[j]> key)
+        {
+            nums[j+1] = nums[j];
+            j--;
+        }
+        nums[j+1] = key;
+    }
+    return nums;
+ }
+   
+};
+
+int main()
+{
+ selection s;
+
+/* selection sort
+ int  n;
+ cout<<"Enter the number of elements in the array: ";
+ cin>>n;
+ vector<int> nums(n);
+ for(int i=0;i<n;i++)
+ {
+    cin>>nums[i];
+ }
+ s.selectionsort(nums);    
+cout<<"sorted arrey:";
+ for(int x: nums)
+ {
+  cout<<x<<" ";
+ }
+  */ 
+
+ /* Bubble sort */
+
+ int n;
+cout<<"Enter the number of elements in the array: ";
+ cin>>n;
+
+ vector<int> b(n);
+
+ for(int i=0;i<n;i++)
+ {
+   cin>>b[i];
+ }
+
+ s.insertionsort(b);
+ 
+cout<<"sorted arrey:";
+ for(int x: b)
+ {
+    cout<<x<<" ";
+ }
+
+ return 0;
+}
